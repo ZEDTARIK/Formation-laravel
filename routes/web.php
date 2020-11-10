cb7c28c557826589fb568ee4051c857ee7d8ef05
ettarak <ettarak.zouhair@gmail.com>
2020-11-10 21:21:40 +0100

    Create Controller and Routing resource

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-/*
-Route::get('/home', function() {
-    return view('home');
-});
-*/
-
 Route::view('/home', 'home');
-
-Route::get('/posts/{id?}', 'PostController@post');
+Route::resource('/post', 'PostController')->only(['index', 'show']);
