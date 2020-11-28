0eb02b238ee4efc95c5dad69777675c02c8945fa
ettarak <ettarak.zouhair@gmail.com>
2020-11-28 17:19:56 +0100

    Coding , Add Package mix npm i and npm run dev

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('/css/theme.css') }}">
     <title>Laravel</title>
 </head>
 <body>
@@ -16,5 +18,6 @@
     
     @yield('content')
 
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
