1d5a4ac19703a013e0ba32248d434edbc2397ac3
ettarak <ettarak.zouhair@gmail.com>
2020-12-02 21:16:04 +0100

    Coding, Relation One to Many Leasy and Eagger

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable  = ['title', 'body', 'slug', 'active'];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
